11d686216c57353c10c6e8e1318fc30607e57f1b
Surreal Gee <94802496+surrealgee@users.noreply.github.com>
2023-12-17 12:05:03 -0400

    Build sketch fuctionality

--- main.js
+++ main.js
@@ -1,15 +1,38 @@
 const grid = document.querySelector('.grid-box');
+const sizeBtn = document.querySelector('#size-btn');
+let gridSize = 16 * 16;
 
-function createGrid() {
+
+grid.addEventListener('mouseover', (e) => {
+    e.stopPropagation();
+    markGrid(e.target.id);
+})
+
+sizeBtn.addEventListener('click', () => {
+    const chosenSize = +prompt("Enter your desired Grid Size: ", 16)
+    const newSize = changeGridSize(chosenSize);
+    grid.innerHTML = "";
+    createGrid(newSize);
+})
+
+function changeGridSize(size) {
+    return size * size
+}
+
+function createGrid(gridSize) {
     let squareNodes = "";
-    let gridSize = 16 * 16;
 
     for (let i = 0; i < gridSize; i++) {
-        squareNodes += `<div class="square"></div>`
+        squareNodes += `<div class="square" id=${"sq" + i}></div>`
     }
 
     grid.innerHTML = squareNodes;
 }
 
+function markGrid(target) {
+    let gridSquare = document.getElementById(target);
+    gridSquare.classList.toggle('clicked');
+}
 
-createGrid();
+
+createGrid(gridSize);
